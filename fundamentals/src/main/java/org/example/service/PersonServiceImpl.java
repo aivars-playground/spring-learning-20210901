865ee46e292d80503675be319c90be733b8c9737
Aivars Smausktelis <aivars.smaukstelis@gmail.com>
2021-09-02 09:41:37 +0300

    bean lifecycle annotations

--- fundamentals/src/main/java/org/example/service/PersonServiceImpl.java
+++ fundamentals/src/main/java/org/example/service/PersonServiceImpl.java
@@ -3,9 +3,9 @@ package org.example.service;
 import org.example.model.Person;
 import org.example.repository.PersonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.PostConstruct;
 import java.util.List;
 
 @Service("personService")
@@ -21,5 +21,10 @@ public class PersonServiceImpl implements PersonService {
     @Override
     public List<Person> findAll() {
         return personRepository.findAll();
+    }
+
+    @PostConstruct
+    private void postConstruct() {
+        System.out.println("postConstruct" + this);
     }
 }
