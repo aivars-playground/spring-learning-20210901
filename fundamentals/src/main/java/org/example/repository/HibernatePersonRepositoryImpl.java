57843ed6fbe5834c2a0aed71473c8342860effd0
Aivars Smausktelis <aivars.smaukstelis@gmail.com>
2021-09-02 11:04:43 +0300

    adding SpEL (Spring Expression Language) expression

--- fundamentals/src/main/java/org/example/repository/HibernatePersonRepositoryImpl.java
+++ fundamentals/src/main/java/org/example/repository/HibernatePersonRepositoryImpl.java
@@ -3,6 +3,7 @@ package org.example.repository;
 import org.example.model.Person;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
@@ -18,13 +19,16 @@ public class HibernatePersonRepositoryImpl implements PersonRepository {
     @Qualifier("calendarTomorrow")
     private Calendar calendarTomorrow;
 
+    @Value("#{T(java.lang.Math).random()}")
+    private double rand;
+
     @Override
     public List<Person> findAll() {
 
         List<Person> persons = new ArrayList<>();
 
         Person person = new Person();
-        person.setFirstName("FN");
+        person.setFirstName("FN:"+rand);
         person.setLastName("LN");
 
         persons.add(person);
