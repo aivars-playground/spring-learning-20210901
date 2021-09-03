7b26dfcce2516aa4e5a4b97435e7e89b903bd3ed
Aivars Smausktelis <aivars.smaukstelis@gmail.com>
2021-09-03 09:44:10 +0300

    initial commit for bz

---
+++ bugzilla/src/main/java/com/example/bugzilla/BugzillaApplication.java
@@ -0,0 +1,13 @@
+package com.example.bugzilla;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BugzillaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BugzillaApplication.class, args);
+    }
+
+}
