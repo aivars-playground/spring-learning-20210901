c72b1a917b6044b7f8fc24ab015b28b7b4fdc981
Aivars Smausktelis <aivars.smaukstelis@gmail.com>
2021-09-05 10:30:34 +0300

    adding api

--- bugtracker/src/main/java/com/example/bugtracker/controller/ApiController.java
+++ bugtracker/src/main/java/com/example/bugtracker/controller/ApiController.java
@@ -2,9 +2,12 @@ package com.example.bugtracker.controller;
 
 import com.example.bugtracker.entity.Application;
 import com.example.bugtracker.repository.ApplicationRepository;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 @RequestMapping("api/v1")
@@ -16,8 +19,16 @@ public class ApiController {
         this.applicationRepository = applicationRepository;
     }
 
-    @GetMapping("application")
+    @GetMapping("applications")
     public Iterable<Application> listApplications() {
         return applicationRepository.findAll();
+    }
+
+
+    @GetMapping("applications/{id}")
+    public Application read(@PathVariable long id) {
+        return applicationRepository
+                .findById(id)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "app not found"));
     }
 }
