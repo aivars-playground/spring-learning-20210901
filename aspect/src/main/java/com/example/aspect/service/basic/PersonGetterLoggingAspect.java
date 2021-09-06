41e78a81f99011a8d3f4d1ae7c73b27061ca821e
Aivars Smausktelis <aivars.smaukstelis@gmail.com>
2021-09-06 21:17:47 +0300

    adding basic aspect with more versatile around method

--- aspect/src/main/java/com/example/aspect/service/basic/PersonGetterLoggingAspect.java
+++ aspect/src/main/java/com/example/aspect/service/basic/PersonGetterLoggingAspect.java
@@ -1,6 +1,8 @@
 package com.example.aspect.service.basic;
 
+import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
@@ -13,15 +15,24 @@ public class PersonGetterLoggingAspect {
 
     private static Logger LOGGER = Logger.getLogger(PersonGetterLoggingAspect.class.getName());
 
-
-
     @Before("execution(* *.getPerson(..))")
     public void before() {
         LOGGER.info("===========Entering method");
     }
+
     @After("execution(* *.getPerson(..))")
     public void after() {
         LOGGER.info("===========Exiting method");
+    }
+
+    @Around("execution(* *.getPerson(..))")
+    public Object around(ProceedingJoinPoint joinPoint) throws Throwable {
+        String name = joinPoint.toLongString();
+        Object args = joinPoint.getArgs();
+        LOGGER.info("===========around pre invocation name:"+name +" args"+args);
+        Object res = joinPoint.proceed();
+        LOGGER.info("===========around post invocation returning:"+res);
+        return res;
     }
 
 
