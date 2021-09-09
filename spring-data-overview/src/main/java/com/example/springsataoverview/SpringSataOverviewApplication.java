a84c066c45f5f4af0b00515d9b363a0f6349759d
Aivars Smausktelis <aivars.smaukstelis@gmail.com>
2021-09-09 14:54:18 +0300

    initial commit

---
+++ spring-data-overview/src/main/java/com/example/springsataoverview/SpringSataOverviewApplication.java
@@ -0,0 +1,13 @@
+package com.example.springsataoverview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSataOverviewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSataOverviewApplication.class, args);
+    }
+
+}
---
+++ spring-data-overview/src/test/java/com/example/springsataoverview/SpringSataOverviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springsataoverview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSataOverviewApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
